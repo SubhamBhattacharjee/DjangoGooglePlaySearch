1d857f343f3063cbbe1b8b4ef951a5023bc323eb
Subham Bhattacharjee <subham.bhattacharjee1@gmail.com>
2017-10-08 01:02:14 +0530

    Models created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+class SearchString(models.Model):
+	searchstring = models.CharField(max_length=250)
+	searched_time = models.DateTimeField(auto_now=True)
+	amount_of_time_searched = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.searchstring + ' - ' + self.searched_time + ' - ' + self.amount_of_time_searched
+
+class Result(models.Model):
+	searched_by = models.CharField(max_length=250)
+	app_id = models.CharField(max_length=250)
+	app_name = models.CharField(max_length=250)
+	dev_name = models.CharField(max_length=250)
+	app_icon = models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.searched_time + ' - ' + self.app_id + ' - ' + self.app_name + ' - ' + self.dev_name + ' - ' + self.app_icon
--- main/views.py
+++ main/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
 
 def index(request):
     template = loader.get_template('index.html')
@@ -14,20 +13,11 @@ def search(request):
 		url  = 'https://play.google.com/store/search?q='+request.POST.get('qry')+'&c=apps'
 		raw  = requests.get(url)
 		soup = BeautifulSoup(raw.text, 'html.parser')
-		# apps = soup.find("div", {"class": "card-list"})
 		apps = soup.find_all(class_="card-content")
-		# print(apps[0].prettify())
 		for ids in apps:
 			app_id = ids.find("span", {"class": "preview-overlay-container"})['data-docid']
 			app_name = ids.find("img", {"class": "cover-image"})['alt']
 			cover_image = ids.find("img", {"class": "cover-image"})['src']
 			dev_name = ids.find(class_="subtitle").get_text()
-			app_details = pd.DataFrame({
-		        "app_id": [app_id], 
-		        "app_name": [app_name], 
-		        "cover_image": [cover_image], 
-		        "dev_name":[dev_name]
-		    })
-			print (app_details)
 
-		return HttpResponse("<pre>"+app_details+"</pre>")
+		return HttpResponse("<pre>lool</pre>")
